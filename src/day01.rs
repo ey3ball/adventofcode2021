5d1bd43cbf81ffe141846bcf865dfbef17cfb225
Florian Vallée <florian@vallee.xyz>
2021-12-01 09:46:56 +0100

    project setup + day 01

---
+++ src/day01.rs
@@ -0,0 +1,37 @@
+use itertools::Itertools;
+
+#[aoc_generator(day1)]
+pub fn generator(input: &str) -> Vec<i32> {
+    input.lines().map(|x| x.parse().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(depths: &Vec<i32>) -> usize {
+    let d1 = &depths[0..depths.len() - 1];
+    let d2 = &depths[1..depths.len()];
+
+    d1.iter()
+        .zip(d2.iter())
+        .map(|(x,y)| y - x)
+        .filter(|&x| x > 0)
+        .count()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(depths: &Vec<i32>) -> usize {
+    let windowed: Vec<(i32, i32, i32)> =
+        depths
+            .iter()
+            .copied()
+            .tuple_windows()
+            .collect();
+
+    let wd1 = &windowed[0..windowed.len() - 1];
+    let wd2 = &windowed[1..windowed.len()];
+
+    wd1.iter()
+        .zip(wd2.iter())
+        .map(|(x,y)| y.2 + y.1 + y.0 - x.2 - x.1 - x.0)
+        .filter(|&x| x > 0)
+        .count()
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+pub mod day01;
+
+aoc_lib! { year = 2021 }
