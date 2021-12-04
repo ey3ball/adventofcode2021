b44aa5be37b3be1d1511e3f4f2aae70b1529110e
Florian Vallée <florian@vallee.xyz>
2021-12-04 15:58:51 +0100

    day4 - part 1

---
+++ src/day04.rs
@@ -0,0 +1,106 @@
+#[derive(Clone)]
+pub struct Card {
+    rows: usize,
+    cols: usize,
+    grid: Vec<u32>,
+    check: Vec<bool>,
+    win: Option<usize>,
+}
+
+type Bingo = (Vec<u32>, Vec<Card>);
+
+impl Card {
+    fn new(grid: Vec<u32>, rows: usize, cols: usize) -> Card {
+        return Card {
+            rows,
+            cols,
+            check: vec![false; grid.len()],
+            grid,
+            win: None,
+        };
+    }
+
+    fn coords(&self, index: usize) -> (usize, usize) {
+        let col = index % self.cols;
+        (col, (index - col) / self.rows)
+    }
+
+    fn row(&self, index: usize) -> Vec<usize> {
+        let (_, row) = self.coords(index);
+        (0..self.cols).map(|c| row * self.cols + c).collect()
+    }
+
+    fn col(&self, index: usize) -> Vec<usize> {
+        let (col, _) = self.coords(index);
+        (0..self.rows).map(|r| r * self.cols + col).collect()
+    }
+
+    fn filled(&self, indexes: Vec<usize>) -> bool {
+        indexes.iter().all(|&i| self.check[i])
+    }
+
+    fn bingo(&mut self, number: u32) -> bool {
+        let pos = self.grid.iter().position(|&item| item == number);
+        match pos {
+            Some(index) => {
+                self.check[index] = true;
+                if self.filled(self.col(index)) || self.filled(self.row(index)) {
+                    self.win = Some(index);
+                }
+                self.win.is_some()
+            }
+            None => false,
+        }
+    }
+}
+
+#[aoc_generator(day4)]
+pub fn generator(input: &str) -> Bingo {
+    let mut lines = input.lines();
+    let draw = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|c| c.parse().unwrap())
+        .collect();
+    lines.next();
+
+    let cols = lines.clone().next().unwrap().split_whitespace().count();
+    let rows = lines.clone().take_while(|&r| r != "").count();
+
+    let cards: Vec<Card> = lines
+        .filter(|&r| r != "")
+        .collect::<Vec<&str>>()
+        .chunks(rows)
+        .map(|card| {
+            card.iter()
+                .flat_map(|row| row.split_whitespace().map(|n| n.parse().unwrap()))
+                .collect::<Vec<u32>>()
+        })
+        .map(|grid| Card::new(grid, rows, cols))
+        .collect();
+
+    (draw, cards)
+}
+
+#[aoc(day4, part1)]
+pub fn part1((draw, cards): &Bingo) -> u32 {
+    let mut cards = cards.clone();
+    let mut bag = draw.iter();
+    let mut has_winner = false;
+    let mut drawn = 0;
+    while !has_winner {
+        drawn = *bag.next().unwrap();
+        has_winner = cards.iter_mut().any(|c| c.bingo(drawn));
+    }
+
+    let winner = cards.iter().find(|c| c.win.is_some()).unwrap();
+    winner
+        .grid
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| !winner.check[i])
+        .map(|(_, v)| v)
+        .sum::<u32>()
+        * drawn
+}
