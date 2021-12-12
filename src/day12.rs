fe123b1978ffda5782ef59da736e939fdcbb5ee7
Florian Vallée <fvallee@fieldbox.ai>
2021-12-12 21:44:42 +0100

    cargo fmt

--- src/day12.rs
+++ src/day12.rs
@@ -1,18 +1,16 @@
 use std::collections::HashMap;
-type Caves<'a> = HashMap<&'a str, Vec<&'a str>,>;
+type Caves<'a> = HashMap<&'a str, Vec<&'a str>>;
 
 pub fn parse(input: &str) -> Caves {
     let mut caves: Caves = HashMap::new();
     caves.insert("end", vec![]);
-    input
-        .lines()
-        .for_each(|l| {
-            let (from, to) = l.split_once("-").unwrap();
-            caves.entry(from).or_insert(vec![]).push(to);
-            if to != "end" && from != "start" {
-                caves.entry(to).or_insert(vec![]).push(from);
-            }
-        });
+    input.lines().for_each(|l| {
+        let (from, to) = l.split_once("-").unwrap();
+        caves.entry(from).or_insert(vec![]).push(to);
+        if to != "end" && from != "start" {
+            caves.entry(to).or_insert(vec![]).push(from);
+        }
+    });
     caves
 }
 
@@ -21,31 +19,33 @@ pub fn explore<'a>(input: &'a Caves, twice: bool) -> Vec<Vec<&'a str>> {
 
     loop {
         let mut next_paths: Vec<(bool, Vec<&str>)> = vec![];
-        paths
-            .iter()
-            .for_each(|(detour_taken, path)| {
-                let last = path.iter().last().unwrap();
-                if last == &"end" {
-                    next_paths.push((*detour_taken, path.clone()));
-                } else {
-                    let options = &input[last];
-                    options
-                        .iter()
-                        .filter(|next| next == &&next.to_ascii_uppercase() || !path.contains(next) || !detour_taken)
-                        .filter(|next| next != &&"start")
-                        .for_each(|valid_next| {
-                            let mut new_path = path.clone();
-                            let detour = if new_path.contains(valid_next) && &valid_next.to_ascii_uppercase() != valid_next {
-                                true
-                            } else {
-                                *detour_taken
-                            };
-                            new_path.push(valid_next.clone());
-                            next_paths.push((detour, new_path));
-                        });
-                }
-            });
-        
+        paths.iter().for_each(|(detour_taken, path)| {
+            let last = path.iter().last().unwrap();
+            if last == &"end" {
+                next_paths.push((*detour_taken, path.clone()));
+            } else {
+                let options = &input[last];
+                options
+                    .iter()
+                    .filter(|next| {
+                        next == &&next.to_ascii_uppercase() || !path.contains(next) || !detour_taken
+                    })
+                    .filter(|next| next != &&"start")
+                    .for_each(|valid_next| {
+                        let mut new_path = path.clone();
+                        let detour = if new_path.contains(valid_next)
+                            && &valid_next.to_ascii_uppercase() != valid_next
+                        {
+                            true
+                        } else {
+                            *detour_taken
+                        };
+                        new_path.push(valid_next.clone());
+                        next_paths.push((detour, new_path));
+                    });
+            }
+        });
+
         if paths == next_paths {
             break;
         } else {
